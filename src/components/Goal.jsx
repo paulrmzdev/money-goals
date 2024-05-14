a5a922168ad7f21fa0396047690704626ad760e1
PaulERC <paulrc.06@gmail.com>
2024-05-14 13:31:47 -0600

    Fixing component styles

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -1,4 +1,6 @@
-const Goal = (props) => {
+import PropTypes from "prop-types";
+import ProgressBar from "./ProgressBar";
+function Goal ({progress,goal,name}) {
 
   //Goal card styles
   const goalContainerStyles = {
@@ -9,61 +11,31 @@ const Goal = (props) => {
     margin: 5,
     textAlign: "left"
   }
-
-  const progressContainerStyles = {
-    height: 25,
-    width: '100%',
-    backgroundColor: "#6666",
-    borderRadius: 50,
-    overflow: "hidden"
-  }
-
-  const fillerStyles = {
-    height: '100%',
-    width: `${progressCalc(props.progress,props.goal)}%`,
-    backgroundColor: "#149eca",
-    borderRadius: 'inherit',
-    textAlign: 'right',
-//  transition: 'width 1s ease-in-out',
-  }
-
-  const processLabelStyles = {
-    padding: 5,
-    color: 'white',
-    fontWeight: 'bold',
-  }
-
-
   function formatear(value){
     return new Intl.NumberFormat('es-Mx', { style: 'currency', currency: 'MXN' }).format(
       value,
     )
   }
-  
-  function progressCalc(aportaciones,value){
-      var progress = (aportaciones / value)*100;
-    return progress.toFixed(2);
-  }
-
 
   return <>
   <div className="goal" style={goalContainerStyles}>
     <div style={{display: "flex",alignItems: "center"}}>
     <img width="48" height="48" src="https://img.icons8.com/color/48/coins.png" alt="coins"/>
-      <h2 style={{color: "#149eca"}}>{props.name}</h2>
+      <h2 style={{color: "#149eca"}}>{name}</h2>
     </div>
     <div className="money" style={{display: "flex", justifyContent: "space-between"}}>
-      <p style={{fontWeight: "bold"}}>{formatear(props.goal)}</p>
-      <p style={{textAlign: "right"}}>{formatear(props.goal-props.progress)}</p>
+      <p style={{fontWeight: "bold"}}>{formatear(goal)}</p>
+      <p style={{textAlign: "right"}}>{formatear(goal-progress)}</p>
     </div>
-    <div className="progressContainer" style={progressContainerStyles}>
-      <div className="filler" style={fillerStyles}>
-        <span style={processLabelStyles}>{formatear(props.progress)}&nbsp;({progressCalc(props.progress,props.goal)}%)</span>
-      </div>
-    </div>
+    <ProgressBar goal={goal} progress={progress}/>
   </div>
     </>
 }
 
+Goal.propTypes = {
+  name: PropTypes.string,
+  progress: PropTypes.string,
+  goal: PropTypes.string
+}
 
 export default Goal
